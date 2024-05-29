bad4d3d9217e57faf2486624ecf13315decf69dd
Hugarios <hugoanisio2@gmail.com>
2024-05-29 19:46:33 -0300

    OK!

---
+++ aula_02b/src/components/TrabalhadoComImagens.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Coldplay from '../assets/one piece.jpg'
+
+
+const TrabalhadoComImagens = () => {
+  return (
+    <div>
+        <img src='public\zoro_one_piece.jpg' alt='The Killers'/>
+        <img src={Coldplay} alt='Coldplay'/>
+    </div>
+  )
+}
+
+export default TrabalhadoComImagens
---
+++ revisao_js/exerc_01.js
@@ -0,0 +1,12 @@
+var nome = "luiz";
+const cpf = "058.945.024-70";
+
+function nomeCompleto() {
+    let sobreNome = "Santana";
+
+    console.log(nome);
+    console.log(sobreNome);
+    console.log(cpf);
+}
+
+nomeCompleto();
